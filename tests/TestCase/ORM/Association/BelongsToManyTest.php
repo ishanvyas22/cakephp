ishanvyas22/cakephp

commit c999ae72b534085cd49c5de9abc775dc6470bb32

Improve association property name clash check. (#18859)
Check for name clash even when property name is explicitly set, not just auto generated.

src/ORM/Association.php
@@ -220,7 +220,6 @@ abstract class Association
             'foreignKey',
             'joinType',
             'tableLocator',
-            'propertyName',
             'sourceTable',
             'targetTable',
         ];
@@ -228,6 +227,10 @@ abstract class Association
             if (isset($options[$property])) {
                 $this->{'_' . $property} = $options[$property];
             }
+        }
+
+        if (isset($options['propertyName'])) {
+            $this->setProperty($options['propertyName']);
         }
 
         $this->_className ??= $alias;
@@ -554,6 +557,19 @@ abstract class Association
     {
         $this->_propertyName = $name;
 
+        try {
+            if (in_array($this->_propertyName, $this->_sourceTable->getSchema()->columns(), true)) {
+                $msg = 'Association property name `%s` clashes with field of same name of table `%s`.' .
+                    ' You should specify an alterate name using the `propertyName` option or `setProperty()` method.';
+                trigger_error(
+                    sprintf($msg, $this->_propertyName, $this->_sourceTable->getTable()),
+                    E_USER_WARNING,
+                );
+            }
+        } catch (DatabaseException $e) {
+            // Schema is not yet loaded, can't check for clashes
+        }
+
         return $this;
     }
 
@@ -566,15 +582,7 @@ abstract class Association
     public function getProperty(): string
     {
         if (!isset($this->_propertyName)) {
-            $this->_propertyName = $this->_propertyName();
-            if (in_array($this->_propertyName, $this->_sourceTable->getSchema()->columns(), true)) {
-                $msg = 'Association property name `%s` clashes with field of same name of table `%s`.' .
-                    ' You should explicitly specify the `propertyName` option.';
-                trigger_error(
-                    sprintf($msg, $this->_propertyName, $this->_sourceTable->getTable()),
-                    E_USER_WARNING,
-                );
-            }
+            $this->setProperty($this->_propertyName());
         }
 
         return $this->_propertyName;

tests/TestCase/ORM/Association/BelongsToManyTest.php
@@ -1399,7 +1399,7 @@ class BelongsToManyTest extends TestCase
      */
     public function testPropertyOption(): void
     {
-        $config = ['propertyName' => 'thing_placeholder'];
+        $config = ['propertyName' => 'thing_placeholder', 'sourceTable' => $this->article];
         $association = new BelongsToMany('Thing', $config);
         $this->assertSame('thing_placeholder', $association->getProperty());
     }

tests/TestCase/ORM/Association/BelongsToTest.php
@@ -341,7 +341,7 @@ class BelongsToTest extends TestCase
      */
     public function testPropertyOption(): void
     {
-        $config = ['propertyName' => 'thing_placeholder'];
+        $config = ['propertyName' => 'thing_placeholder', 'sourceTable' => $this->client];
         $association = new BelongsTo('Thing', $config);
         $this->assertSame('thing_placeholder', $association->getProperty());
     }

tests/TestCase/ORM/Association/HasManyTest.php
@@ -663,7 +663,7 @@ class HasManyTest extends TestCase
      */
     public function testPropertyOption(): void
     {
-        $config = ['propertyName' => 'thing_placeholder'];
+        $config = ['propertyName' => 'thing_placeholder', 'sourceTable' => $this->author];
         $association = new HasMany('Thing', $config);
         $this->assertSame('thing_placeholder', $association->getProperty());
     }

tests/TestCase/ORM/Association/HasOneTest.php
@@ -253,7 +253,7 @@ class HasOneTest extends TestCase
      */
     public function testPropertyOption(): void
     {
-        $config = ['propertyName' => 'thing_placeholder'];
+        $config = ['propertyName' => 'thing_placeholder', 'sourceTable' => $this->user];
         $association = new HasOne('Thing', $config);
         $this->assertSame('thing_placeholder', $association->getProperty());
     }

commit a4e71f99b6c64182f35e087bff922693f986eccb

Fix association throws exception when connection is not set or database not present
Fixes regression introduced by PR #18859

src/ORM/Association.php
@@ -20,6 +20,7 @@ use Cake\Collection\CollectionInterface;
 use Cake\Core\App;
 use Cake\Core\ConventionsTrait;
 use Cake\Database\Exception\DatabaseException;
+use Cake\Database\Exception\MissingConnectionException;
 use Cake\Database\Expression\IdentifierExpression;
 use Cake\Database\Expression\QueryExpression;
 use Cake\Database\ExpressionInterface;
@@ -566,7 +567,7 @@ abstract class Association
                     E_USER_WARNING,
                 );
             }
-        } catch (DatabaseException) {
+        } catch (DatabaseException | MissingConnectionException) {
             // Schema is not yet loaded, can't check for clashes
         }
 

tests/TestCase/ORM/Association/BelongsToManyTest.php
@@ -1397,6 +1397,28 @@ class BelongsToManyTest extends TestCase
     }
 
     /**
+     * Tests that property is being set doesn't throw exception when connection is not configured.
+     */
+    public function testPropertyOption_MissingConnectionError(): void
+    {
+        $connection = ConnectionManager::get('test');
+        $this->skipIf($connection->getDriver() instanceof Sqlite, 'Skip for sqlite');
+
+        // Create a dummy database connection to test
+        $originalConfig = ConnectionManager::getConfig('test');
+        ConnectionManager::drop('test');
+        ConnectionManager::setConfig('test', $originalConfig + ['database' => 'nonexistent']);
+
+        $config = ['propertyName' => 'thing_placeholder', 'sourceTable' => $this->article];
+        $association = new BelongsToMany('Thing', $config);
+        $this->assertSame('thing_placeholder', $association->getProperty());
+
+        // Clean up
+        ConnectionManager::drop('test');
+        ConnectionManager::setConfig('test', $originalConfig);
+    }
+
+    /**
      * Test that plugin names are omitted from property()
      */
     public function testPropertyNoPlugin(): void